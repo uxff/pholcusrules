b70a6ac1b50ec2519b76a017ed9483671e330665
uxff <uxff@qq.com>
2018-01-18 23:44:38 +0800

    add duanwenxue

--- app/example.go
+++ app/example.go
@@ -17,6 +17,7 @@ import (
 	"github.com/henrylee2cn/pholcus/runtime/status"
 	"github.com/henrylee2cn/pholcus/web"
 
+	_ "github.com/uxff/pholcusrules/duanwenxue"
 	_ "github.com/uxff/pholcusrules/kuaidaili"
 	_ "github.com/uxff/pholcusrules/picsetcrawler"
 	_ "github.com/uxff/pholcusrules/qzone"
