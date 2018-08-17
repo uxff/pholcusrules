2450c798754e80600439abf41beffb5c6be680c7
uxff <uxff@qq.com>
2018-08-17 23:14:21 +0800

    simplify translator

--- langtranslate/transinterface.go
+++ langtranslate/transinterface.go
@@ -14,48 +14,46 @@ const (
 	LANG_RU   LANG_TYPE = "ru"
 )
 
-type TRANSLATOR_ID int
-
 const (
 	// baidu: http://api.fanyi.baidu.com/api/trans/product/apidoc
-	TRANSLATOR_BAIDU TRANSLATOR_ID = iota
+	TRANSLATOR_BAIDU = iota + 1
 	// google:
-	TRANSLATOR_GOOGLE TRANSLATOR_ID = iota
+	TRANSLATOR_GOOGLE
 	// youdao chrome extension:
-	TRANSLATOR_YOUDAO_FREE TRANSLATOR_ID = iota
+	TRANSLATOR_YOUDAO_FREE
 	// youdao:
-	TRANSLATOR_YOUDAO TRANSLATOR_ID = iota
+	TRANSLATOR_YOUDAO
 	// microsoft azure: https://docs.microsoft.com/en-us/azure/cognitive-services/translator/
-	TRANSLATOR_MICROSOFT TRANSLATOR_ID = iota
+	TRANSLATOR_MICROSOFT
 	// xunfei: http://www.xfyun.cn/services/mtranslate
-	TRANSLATOR_XFYUN TRANSLATOR_ID = iota
+	TRANSLATOR_XFYUN
 	// tencent: https://cloud.tencent.com/document/product/551
-	TRANSLATOR_TENCENT TRANSLATOR_ID = iota
+	TRANSLATOR_TENCENT
 	// aliyun: no translator api support, but third part supported
-	//TRANSLATOR_ALIYUN TRANSLATOR_ID = iota // use iciba youdao baidu//
-	//
-	TRANSLATOR_ICIBA TRANSLATOR_ID = iota
+	//TRANSLATOR_ALIYUN  = iota // use iciba youdao baidu//
+	// aiciba
+	TRANSLATOR_ICIBA
 	// yeecloud: https://market.aliyun.com/products/57124001/cmapi014709.html?spm=5176.730005.0.0.mUzPbY
-	TRANSLATOR_YEECLOUD TRANSLATOR_ID = iota
+	TRANSLATOR_YEECLOUD
 )
 
 type Translator interface {
+	// todo: SetApiConfig(map[interface{}]interface{})
 	SetApiConfig(map[string]interface{})
 	SetFromLang(lang string)
 	SetToLang(lang string)
 	Translate(str string) (string, error)
-	AsyncTranslate(str string) <-chan *TransRes
-	GetTransResult(int) (string, error)
-	//AddTranslateTask(str string) *TranslatorTask
 }
 
 type TransRes struct {
-	Res string
-	Err error
+	Res            string
+	Err            error
+	AssumeFromLang string
+	AssumeToLang   string
 }
 
-func SelectTranslator(id TRANSLATOR_ID) Translator {
-	switch id {
+func SelectTranslator(translatorId int) Translator {
+	switch translatorId {
 	case TRANSLATOR_BAIDU:
 		return &BaiduTranslator{}
 	case TRANSLATOR_GOOGLE:
