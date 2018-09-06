18411d030893a573106c4f474d09c72889ac1bd4
uxff <uxff@qq.com>
2018-09-06 23:55:22 +0800

    update main, exit after run in cmd mode

--- app/example.go
+++ app/example.go
@@ -9,11 +9,13 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/henrylee2cn/pholcus/app"
 	"github.com/henrylee2cn/pholcus/cmd"
 	"github.com/henrylee2cn/pholcus/common/gc"
 	"github.com/henrylee2cn/pholcus/config"
+	"github.com/henrylee2cn/pholcus/logs" //信息输出
 	"github.com/henrylee2cn/pholcus/runtime/cache"
 	"github.com/henrylee2cn/pholcus/runtime/status"
 	"github.com/henrylee2cn/pholcus/web"
@@ -163,8 +165,13 @@ func run(ui string) {
 	switch ui {
 	case "web":
 		go web.Run()
+		<-ctrl
 	case "cmd":
+		logs.Log.Notice("cmd run start")
+
 		cmd.Run()
+		logs.Log.Notice("cmd run over")
+		time.Sleep(time.Second)
 	}
 
 	/*
@@ -174,7 +181,6 @@ func run(ui string) {
 		}
 	*/
 
-	<-ctrl
 }
 
 func writeFlag() {
